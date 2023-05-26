cecefde00ad0c5d0135a8c976c0b9d7dd2cb10da
Klaus Moser <klaus.moser@fujitsu.com>
2023-05-26 15:13:31 +0200

    Add 2 test components + authGuard

--- keycloac-test-app/src/app/app-routing.module.ts
+++ keycloac-test-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicSpaceComponent } from './components/public-space/public-space.component';
+import { RestrictedSpaceComponent } from './components/restricted-space/restricted-space.component';
+import { AuthGuard } from './keycloakauth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', redirectTo: '/' },
+  { path: 'public', component: PublicSpaceComponent },
+  { path: 'restricted', component: RestrictedSpaceComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- keycloac-test-app/src/app/app.module.ts
+++ keycloac-test-app/src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { NgModule } from '@angular/core';
+// modules
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+// components
 import { AppComponent } from './app.component';
+import { PublicSpaceComponent } from './components/public-space/public-space.component';
+import { RestrictedSpaceComponent } from './components/restricted-space/restricted-space.component';
+
+
+// init keycloak on app start-up
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080', // url
+        realm: 'test-realm',          // realm name
+        clientId: 'test-client'       // client id
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicSpaceComponent,
+    RestrictedSpaceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
